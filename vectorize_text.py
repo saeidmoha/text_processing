713969832186d528167ace3836c4ba0dea148391
M <saeid@saeidm.com>
2017-11-15 16:43:54 -0800

    text processing (vectorizer)

---
+++ vectorize_text.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+
+import os
+import pickle
+import re
+import sys
+
+sys.path.append( "../tools/" )
+from parse_out_email_text import parseOutText
+
+"""
+    Starter code to process the emails from Sara and Chris to extract
+    the features and get the documents ready for classification.
+
+    The list of all the emails from Sara are in the from_sara list
+    likewise for emails from Chris (from_chris)
+
+    The actual documents are in the Enron email dataset, which
+    you downloaded/unpacked in Part 0 of the first mini-project. If you have
+    not obtained the Enron email corpus, run startup.py in the tools folder.
+
+    The data is stored in lists and packed away in pickle files at the end.
+"""
+
+
+from_sara  = open("from_sara.txt", "r")
+from_chris = open("from_chris.txt", "r")
+
+from_data = []
+word_data = []
+
+### temp_counter is a way to speed up the development--there are
+### thousands of emails from Sara and Chris, so running over all of them
+### can take a long time
+### temp_counter helps you only look at the first 200 emails in the list so you
+### can iterate your modifications quicker
+#temp_counter = 0
+
+
+for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
+    #print ("name = ", name, "from_person", from_person)
+    temp_counter = 0
+    for path in from_person:
+        ### only look at first 200 emails when developing
+        ### once everything is working, remove this line to run over full dataset
+        temp_counter += 1
+        #if temp_counter < 200: # just used during development
+        if temp_counter > 0:
+            path = os.path.join('..', path[:-1])
+            #print (path)
+            email = open(path, "r")
+
+            ### use parseOutText to extract the text from the opened email
+            text = parseOutText(email)
+
+            ### use str.replace() to remove any instances of the words
+            ### ["sara", "shackleton", "chris", "germani"]
+            for word in ["sara", "shackleton", "chris", "germani", "sshacklensf", "cgermannsf"]:
+                text = text.replace(word, '')
+            #print (text)
+
+            ### append the text to word_data
+            word_data.append(text)
+
+            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+            if (name == "sara"):
+                from_data.append('0')
+            else:
+                from_data.append('1')
+
+
+            email.close()
+
+print ("emails processed")
+from_sara.close()
+from_chris.close()
+
+print ("word_data[152] is:", word_data[152])
+
+pickle.dump( word_data, open("your_word_data.pkl", "wb") )
+pickle.dump( from_data, open("your_email_authors.pkl", "wb") )
+
+
+### in Part 4, do TfIdf vectorization here
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+vect = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
+                             stop_words='english')
+#vect = TfidfVectorizer(analyzer='word', min_df = 0, stop_words = 'english')
+#vect = TfidfVectorizer(sublinear_tf=True, max_df=0.5, analyzer='word', 
+#           stop_words='english', vocabulary=vocabulary)
+vect.fit_transform(word_data)
+feature_names = vect.get_feature_names() 
+print("nb_of_words", len(feature_names) )
+#print("word number 34597 is=", feature_names[34595:34599] )
+print("word number 34597 is=", feature_names[34597] )
+print("word number 33614 is=", feature_names[33614] ) # needed for the next project
+
+for index, item in enumerate(feature_names):
+    if index == 33614:        
+        print (item)
+
+print(vect.get_feature_names()[33614])
+
+
